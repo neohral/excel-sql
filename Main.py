f9cbbca870788160e88758fbfcdfe1d1611bcdc4
neohral <honekingu29@gmail.com>
2024-03-29 09:22:08 +0900

    mod

--- Main.py
+++ Main.py
@@ -1,28 +1,65 @@
 import openpyxl
 import sys
 import datetime
+import configparser
+
+#設定ファイル読み込み
+config = configparser.RawConfigParser()
+config.read('settings.ini')
 
 dt_now = datetime.datetime.now()
 
+#設定ファイル読み込み
 args = sys.argv
-HEAD_LINE = 1
+if(len(args)==2):
+    env='DEFAULT'
+else:
+    env=args[2]
+
+HEAD_LINE = int(config[env]['HEAD_LINE'])
+OPTION_LINE = int(config[env]['OPTION_LINE'])
+NULL_STRING = config[env]['NULL_STRING']
+
+#エクセルファイル読み込み
 wb = openpyxl.load_workbook(args[1], data_only=True)
 ws_list = wb.worksheets
 ws = wb.worksheets[0]
 headerStr = ''
 record = ''
+
+#テーブル名(シート名)
 tableName = ws.title
 fileName=f'output/{tableName}_{dt_now.strftime('%Y%m%d%H%M%S')}_insert.sql'
 f = open(fileName, 'a')
-for row in ws.rows:
-    recordList = []
-    for cell in row:
+
+opList = {}
+colList = []
+recList = []
+for col in ws.iter_cols():
+    for cell in col:
         if(HEAD_LINE == cell.row):
-            recordList.append(str(cell.value))
-        else:
-            recordList.append(f'\'{str(cell.value)}\'')
-    if(HEAD_LINE == cell.row):
-        headerStr = f'INSERT INTO {tableName} ({','.join(recordList)}) VALUES ('
-    else:
-        record = f'{headerStr}{','.join(recordList)});'
-        f.write(record+'\n')
+            colList.append(str(cell.value))
+            opList[str(cell.value)]=["nullcheck","string"]
+        elif(OPTION_LINE == cell.row):
+            currentColmun=ws.cell(HEAD_LINE,cell.column).value
+            opList[currentColmun]=str(cell.value).split(',')
+            
+for row in ws.iter_rows():
+    onerec = []
+    for cell in row:
+        if(HEAD_LINE != cell.row and OPTION_LINE != cell.row):
+            planeRecord = str(cell.value)
+            currentColmun=ws.cell(int(HEAD_LINE),cell.column).value
+            if("string" in opList[currentColmun]):
+                planeRecord = f'\'{str(cell.value)}\''
+            if("nullcheck" in opList[currentColmun]):
+                if(NULL_STRING==cell.value):
+                    planeRecord = str(cell.value)
+            onerec.append(planeRecord)
+    if(len(onerec) != 0):
+        recList.append(onerec)
+
+for rec in recList:
+    headerStr = f'INSERT INTO {tableName} ({','.join(colList)}) VALUES ('
+    record = f'{headerStr}{','.join(rec)});'
+    print(record)
